7a2d47e81d773ce02779b6ee8cab478534db123e
Alec Mac <alecmac95@gmail.com>
2021-04-12 15:10:15 -0700

    update color fragment to change text to color/hex

--- app/src/main/java/edu/uw/tcss450/dbest01/ui/ColorFragment.java
+++ app/src/main/java/edu/uw/tcss450/dbest01/ui/ColorFragment.java
@@ -1,5 +1,6 @@
 package edu.uw.tcss450.dbest01.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -24,19 +25,39 @@ public class ColorFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentColorBinding.inflate(inflater,container,false);
+        binding = FragmentColorBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         ColorFragmentArgs args = ColorFragmentArgs.fromBundle((getArguments()));
 
         binding.textLabel.setTextColor(args.getColor());
+        updateColorTextLabel(args.getColor());
     }
-    private void updateContent(int color){
+
+    private void updateContent(int color) {
         binding.textLabel.setTextColor(color);
+    }
+
+    private void updateColorTextLabel(int colorInt) {
+        String colorString = String.format("#%06X", 0xFFFFFF & colorInt);
+        switch (colorInt) {
+            case Color.RED:
+                binding.textLabel.setText(R.string.text_red);
+                break;
+            case Color.BLUE:
+                binding.textLabel.setText(R.string.text_blue);
+                break;
+            case Color.GREEN:
+                binding.textLabel.setText(R.string.text_green);
+                break;
+            default:
+                binding.textLabel.setText(String.format("#%06X", 0xFFFFFF & colorInt));
+                break;
+        }
     }
 }
